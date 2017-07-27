ed612c3f473db9aed869decbf8df11b9f56fede2
colin <colin1124x@gmail.com>
2017-07-27 15:39:29 +0800

    .

--- main.go
+++ main.go
@@ -13,17 +13,23 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+const (
+	LOG_DEBUG = 1 << iota
+)
+
 var (
 	version string
 	env     struct {
 		Addr       string
 		ConfigFile string
 	}
+	logLevel    int
 	showVersion bool
 )
 
 func init() {
 
+	flag.IntVar(&logLevel, "log", 0, "log level (0-1)")
 	flag.BoolVar(&showVersion, "v", false, "display version")
 	flag.StringVar(&env.Addr, "addr", ":6666", "http listen address")
 	flag.StringVar(&env.ConfigFile, "conf", "/etc/ssh-agent-server.conf", "config file for authorized keys (use csv format)")
@@ -78,7 +84,9 @@ func main() {
 			buf.WriteString(key + "\n")
 		}
 		w.Write(buf.Bytes())
-		log.Println(buf.String())
+		if logLevel&LOG_DEBUG > 0 {
+			log.Println(buf.String())
+		}
 	})
 
 	log.Println(http.ListenAndServe(env.Addr, router))
